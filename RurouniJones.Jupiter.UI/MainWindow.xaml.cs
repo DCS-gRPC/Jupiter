810d3e908ff7e35930c15ebc906d2305f244291c
Jeffrey Jones <jeff@jones.be>
2021-07-28 14:35:41 +0900

    Add Map Interface
    Add a minimal Map interface with a context menu proof-of-concept that
    shows we can get the information we need to update the DCS mission.

--- RurouniJones.Jupiter.UI/MainWindow.xaml.cs
+++ RurouniJones.Jupiter.UI/MainWindow.xaml.cs
@@ -1,28 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MapControl;
+using MapControl.Caching;
 
 namespace RurouniJones.Jupiter.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public UIElement CurrentMapLayer => new MapTileLayer
+        {
+            TileSource = new TileSource {UriFormat = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"},
+            SourceName = "OpenStreetMap",
+            Description = "© [OpenStreetMap contributors](http://www.openstreetmap.org/copyright)"
+        };
+
+        public Location MouseLocation;
+
+        static MainWindow()
+        {
+            try
+            {
+                ImageLoader.HttpClient.DefaultRequestHeaders.Add("User-Agent", "DCS.Jupiter");
+
+                TileImageLoader.Cache = new ImageFileCache(TileImageLoader.DefaultCacheFolder);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            DataContext = this;
+
+            if (TileImageLoader.Cache is ImageFileCache cache)
+            {
+                Loaded += async (s, e) =>
+                {
+                    await Task.Delay(2000);
+                    await cache.Clean();
+                };
+            }
+        }
+
+        private void MainMap_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MouseLocation = MainMap.ViewToLocation(e.GetPosition(MainMap)); 
+            ((ContextMenu) MainMap.FindResource("MapContext")).IsOpen = true;
+        }
+
+        private void Flare_OnClick(object sender, RoutedEventArgs e)
+        {
+            var value = (string) ((MenuItem) sender).Header;
+            MessageBox.Show($"Launch {value.ToUpper()} flare at Lat {MouseLocation.Latitude} Lon {MouseLocation.Longitude}");
+        }
+
+        private void Smoke_OnClick(object sender, RoutedEventArgs e)
+        {
+            var value = (string) ((MenuItem) sender).Header;
+            MessageBox.Show($"Pop {value.ToUpper()} smoke at Lat {MouseLocation.Latitude} Lon {MouseLocation.Longitude}");
         }
     }
 }
