f06a7d02597582edf449c702d5d62ed51ba9cc8e
Jeffrey Jones <jeff@jones.be>
2021-11-12 00:43:25 +0900

    Remove the `RurouniJones` prefix
    It lenthens the file paths for no benefit and is annoying.

--- Jupiter.Core/Global.cs
+++ Jupiter.Core/Global.cs
@@ -2,7 +2,7 @@
 {
     public static class Global
     {
-        public static string HostName { get; set; } = "localhost";
+        public static string HostName { get; set; } = "pgaw.hoggitworld.com";
         public static string Port { get; set; } = "50051";
     }
 }
